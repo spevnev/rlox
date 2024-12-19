1dab115c3153c06a736c0552082e95f39477f7e0
spevnev <spevnev16@gmail.com>
2024-12-19 12:38:31 -0500

    Running REPL or interpret based on args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env::args, process::ExitCode};
+
+fn usage(program: &str) {
+    eprintln!("usage:");
+    eprintln!("{program}        - REPL");
+    eprintln!("{program} [path] - interpret file");
+}
+
+fn run_repl() {
+    println!("Running REPL");
+}
+
+fn run_file(path: &str) {
+    println!("Interpreting \"{path}\"");
+}
+
+fn main() -> ExitCode {
+    let args: Vec<String> = args().collect();
+    let program = args[0].split('/').last().unwrap();
+
+    match args.len() {
+        1 => {
+            run_repl();
+            return ExitCode::SUCCESS;
+        }
+        2 => {
+            run_file(&args[1]);
+            return ExitCode::SUCCESS;
+        }
+        _ => {
+            usage(program);
+            return ExitCode::FAILURE;
+        }
+    }
 }
